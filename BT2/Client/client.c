e4e819455da8614b7843ca7a769e5779263b9a36
KhacNgoc <nguyenngoc99uet@gmail.com>
2019-10-09 22:48:00 +0700

    BT2

--- BT2/Client/client.c
+++ BT2/Client/client.c
@@ -1,75 +1,131 @@
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <stdlib.h> 
-#include <netinet/in.h> 
-#include <string.h> 
-#define PORT 8080 
-   
-int main(int argc, char const *argv[]) 
-{ 
-    struct sockaddr_in address; 
-    int sock = 0, valread; 
-    struct sockaddr_in serv_addr; 
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <ctype.h>
+#include <libgen.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include "transfer.h"
+#define PORT 8080
+
+ssize_t total = 0;
+void sendfile(FILE *fp, int sockfd);
+int main(int argc, char const *argv[])
+{
+    int a=0;
+    struct sockaddr_in address;
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
     char *hello = "Hello from client.";
-    char hello1[999]={0};
-    char buffer[1024] = {0}; 
+    char hello1[999] = {0};
+    char buffer[1024] = {0};
     char add[225];
     int _continue = 1;
-    printf("Nhap dia chi server\n");
+    printf("Nhap dia chi server: ");
     gets(add);
     //tao socket
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
-    { 
-        printf("\n Socket creation error \n"); 
-        return -1; 
-    } 
-    memset(&serv_addr, '0', sizeof(serv_addr)); 
-    serv_addr.sin_family = AF_INET; 
-    serv_addr.sin_port = htons(PORT); 
-    // Convert IPv4 and IPv6 addresses from text to binary form 
-    if(inet_pton(AF_INET, add, &serv_addr.sin_addr) <= 0)  
-    { 
-        printf("\nInvalid address/ Address not supported \n"); 
-        return -1; 
-    } 
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, add, &serv_addr.sin_addr) <= 0)
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
     // connect
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
-    { 
-        printf("\nConnection Failed \n"); 
-        return -1; 
-    } 
-    while(_continue == 1){
-        char mess_from_client[225]="Hello server!";
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+    while (_continue == 1)
+    {
         
-        hello = &mess_from_client;
-
-        printf("\n");
-        send(sock , hello , strlen(hello) , 0 ); 
-        // printf("Hello message sent\n"); 
-        valread = read( sock , buffer, 1024); 
-        printf("%s\n",buffer );
-    //     if(strcmp(buffer, "Hello client!\nWhat is your name?") == 0){
-	// 	char name_client[99];
-	// 	printf("Enter your name: ");
-	// 	gets(name_client);
-		
-    //     	fflush(stdin);
-	// 	strcpy(hello1, "My name is ");
-	// 	strcat(hello1, name_client);
-	// 	strcat(hello1, " ! Nice to meet you!\n");
-	// 	printf("%s", hello1);
-		
-	// 	send(sock , hello1 , strlen(hello1) , 0 );
-	// 	close(sock);
-	// 	return 0; 
-		
-	// }
-        if (strcmp(mess_from_client, "bye") == 0)
+        if(a==0)
         {
-            _continue = 0;
+            char mess_from_client[225] = "Hello server!";
+
+            hello = &mess_from_client;
+
+            printf("\n");
+            send(sock, hello, strlen(hello), 0);
+            a++;
+        }
+        valread = read(sock, buffer, 1024);
+        // printf("Hello message sent\n");
+        if (strcmp(buffer, "Hello client!") == 0)
+        {
+            printf("%s\n", buffer);
+            char *confirm = "Are you ready?";
+            send(sock, confirm, strlen(confirm), 0);
+            continue;
+        }
+        else if (strcmp(buffer, "Yes! Ten file muon upload: ") == 0)
+        {
+            printf("%s\n", buffer);
+            char *filename[99];
+            gets(filename);
+            fflush(stdin);
+            if (filename == NULL)
+            {
+                perror("Can't get filename");
+                exit(1);
+            }
+
+            char buff[BUFFSIZE] = {0};
+            strncpy(buff, filename, strlen(filename));
+            if (send(sock, buff, BUFFSIZE, 0) == -1)
+            {
+                perror("Can't send filename");
+                exit(1);
+            }
+
+            FILE *fp = fopen(filename, "rb");
+            if (fp == NULL)
+            {
+                perror("Can't open file");
+                exit(1);
+            }
+
+            sendfile(fp, sock);
+            //puts("Send Success");
+            printf("Gui thanh cong, NumBytes = %ld\n", total);
+            fclose(fp);
+            close(sock);
+            return;
         }
         fflush(stdin);
     }
     close(sock);
-    return 0; 
+    return 0;
+}
+void sendfile(FILE *fp, int sockfd)
+{
+    int n;
+    char sendline[MAX_LINE] = {0};
+    while ((n = fread(sendline, sizeof(char), MAX_LINE, fp)) > 0)
+    {
+        total += n;
+        if (n != MAX_LINE && ferror(fp))
+        {
+            perror("Read File Error");
+            exit(1);
+        }
+
+        if (send(sockfd, sendline, n, 0) == -1)
+        {
+            perror("Can't send file");
+            exit(1);
+        }
+        memset(sendline, 0, MAX_LINE);
+    }
 }
--- BT2/Server/server.c
+++ BT2/Server/server.c
@@ -1,78 +1,137 @@
-#include <unistd.h> 
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <stdlib.h> 
-#include <netinet/in.h> 
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
 #include <string.h>
 #include <ctype.h>
-#define PORT 8080 
-int main(int argc, char const *argv[]) 
-{ 
-    int server_fd, new_socket, valread; 
-    struct sockaddr_in address; 
-    int opt = 1; 
-    int addrlen = sizeof(address); 
+#include <libgen.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include "transfer.h"
+#define PORT 8080
+
+ssize_t total = 0;
+void writefile(int sockfd, FILE *fp);
+int main(int argc, char const *argv[])
+{
+    int a = 0;
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
     char mess_from_client[225];
-    char buffer[1024] = {0}; 
+    char buffer[1024] = {0};
     char *hello = "Hello from server.";
     int continu = 1;
     //tao socket
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
-    { 
-        perror("socket failed"); 
-        exit(EXIT_FAILURE); 
-    }  
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
     //gan dia chi cho socket
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) 
-    { 
-        perror("setsockopt"); 
-        exit(EXIT_FAILURE); 
-    } 
-    address.sin_family = AF_INET; 
-    address.sin_addr.s_addr = INADDR_ANY; 
-    address.sin_port = htons( PORT );  //gan cong la 8080   
-    // bind 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) 
-    { 
-        perror("bind failed"); 
-        exit(EXIT_FAILURE); 
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT); //gan cong la 8080
+    // bind
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
     }
     //listen, chi dinh socket lang nghe ket noi
-    if (listen(server_fd, 3) < 0) 
-    { 
-        perror("listen"); 
-        exit(EXIT_FAILURE); 
-    } 
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
     //accept, chap nhan ket noi
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,(socklen_t*)&addrlen))<0) 
-    { 
-        perror("accept"); 
-        exit(EXIT_FAILURE); 
-    } 
-   while(continu == 1){    
-	    char str_cli_ip[INET_ADDRSTRLEN];
-	    struct sockaddr_in* ip_client = (struct sockaddr_in*)&address;
-	    inet_ntop(AF_INET, &ip_client->sin_addr, str_cli_ip, INET_ADDRSTRLEN);
-	    printf("ipclient: %s\n", str_cli_ip );
-	    char str_cli_port[INET_ADDRSTRLEN];
-	    printf("port: %d\n", ntohs(ip_client->sin_port));
-	    printf("\n");
-	    
-	    valread = read( new_socket, buffer, 1024);
-	    printf("%s\n",buffer );
-	    // if(strcmp(buffer, "Hello server!") == 0){
-		// hello = "Hello client!\nWhat is your name?";
-	    	
-	    // 	send(new_socket, hello, strlen(hello), 0 ); 
-	    // }
-	    // if(strstr(buffer, "My name")){
-		// printf("%s\nBye from client.\n",buffer ); 
-		continu=0;
-    	// 	break; 
-	    // }
-	    
-	    
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    while (continu == 1)
+    {
+        char str_cli_ip[INET_ADDRSTRLEN];
+        struct sockaddr_in *ip_client = (struct sockaddr_in *)&address;
+        inet_ntop(AF_INET, &ip_client->sin_addr, str_cli_ip, INET_ADDRSTRLEN);
+        if (a == 0)
+        {
+            printf("ipclient: %s\n", str_cli_ip);
+            char str_cli_port[INET_ADDRSTRLEN];
+            printf("port: %d\n", ntohs(ip_client->sin_port));
+            printf("\n");
+            a++;
+        }
+        valread = read(new_socket, buffer, 1024);
+        if (strcmp(buffer, "Hello server!")==0)
+        {
+            printf("%s\n", buffer);
+            char *reHello = "Hello client!";
+            send(new_socket, reHello, strlen(reHello), 0);
+            continue;
+        }
+        else if (strcmp(buffer, "Are you ready?")==0)
+        {
+            printf("%s\n", buffer);
+            char *resend = "Yes! Ten file muon upload: ";
+            send(new_socket, resend, strlen(resend), 0);
+            continue;
+        }
+        else
+        {
+            char filename[BUFFSIZE] = {0};
+            if (recv(new_socket, filename, BUFFSIZE, 0) == -1)
+            {
+                perror("Can't receive filename");
+                exit(1);
+            }
+
+            FILE *fp = fopen("filetransfer.txt", "wb");
+            if (fp == NULL)
+            {
+                perror("Can't open file");
+                exit(1);
+            }
+
+            char addr[INET_ADDRSTRLEN];
+            printf("Start receive file: %s from %s\n", filename, inet_ntop(AF_INET, &address.sin_addr, addr, INET_ADDRSTRLEN));
+            writefile(new_socket, fp);
+            printf("Receive Success, NumBytes = %ld\n", total);
+
+            fclose(fp);
+            close(new_socket);
+            return 0;
+        }
     }
     close(new_socket);
-    return 0; 
-} 
+    return 0;
+}
+void writefile(int sockfd, FILE *fp)
+{
+    ssize_t n;
+    char buff[MAX_LINE] = {0};
+    while ((n = recv(sockfd, buff, MAX_LINE, 0)) > 0)
+    {
+        total += n;
+        if (n == -1)
+        {
+            perror("Receive File Error");
+            exit(1);
+        }
+
+        if (fwrite(buff, sizeof(char), n, fp) != n)
+        {
+            perror("Write File Error");
+            exit(1);
+        }
+        memset(buff, 0, MAX_LINE);
+    }
+}
